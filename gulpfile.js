c82e894430f345f710e13a86680996e7ba47a8ac
Leisan <allarepossible@gmail.com>
2016-01-21 22:46:52 +0300

    Second page

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require("gulp"),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync');
 
 // Сервер
@@ -11,6 +13,16 @@ gulp.task('server', function () {
   });
 });
 
+ 
+gulp.task('prefix', function () {
+   gulp.src('css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(rename('prefix.css'))
+    .pipe(gulp.dest('app/css/'));
+});
 // Слежка
 gulp.task('watch', function () {
   gulp.watch([
