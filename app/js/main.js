9058582c5b5c8deede12312c49121b5bfe39d659
Leisan <allarepossible@gmail.com>
2016-01-29 01:23:11 +0300

    popup in my_work.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
-$('input, textarea').placeholder();
+$(document).ready(function() {
+	$('input, textarea').placeholder();
+});
+
+    $(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы    
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
