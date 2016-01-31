4ddaba9fd5a863489d3697035869b253b4e396ac
Leisan <allarepossible@gmail.com>
2016-02-01 02:23:30 +0300

    adaptiv for ipad + popap with upload + support in ie8

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,48 @@ $(document).ready(function() {
 	$('input, textarea').placeholder();
 });
 
-    $(document).ready(function(){
-        //Скрыть PopUp при загрузке страницы    
-        PopUpHide();
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();   
+    $(".op").show();
+
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+    $(".op").hide();
+}
+
+$(function() {
+    var pull        = $('#pull');
+        menu        = $('.nav_list');
+        menuHeight  = menu.height();
+
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
     });
-    //Функция отображения PopUp
-    function PopUpShow(){
-        $("#popup1").show();
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w < 769 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
+
+function getName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
     }
-    //Функция скрытия PopUp
-    function PopUpHide(){
-        $("#popup1").hide();
-    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }                       
+    var filename = str.slice(i);            
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
