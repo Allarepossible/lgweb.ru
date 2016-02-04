6dde47eb7d9d6c54170cc9c46d2fe64f7099c8ea
Leisan <allarepossible@gmail.com>
2016-02-04 03:42:35 +0300

    valid form + tooltips + ie8

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
 	$('input, textarea').placeholder();
+    $('.b-popup').click(PopUpHide);
+    $('.b-popup-content').click(function(event){
+   event.stopPropagation()});
+        $('.socials').clone().appendTo('.wrapper').addClass('socials2').removeClass('socials');
+        $('.empty').clone().appendTo('.wrapper');
+
 });
 
-$(document).ready(function(){
-    //Скрыть PopUp при загрузке страницы    
-    PopUpHide();
-});
 //Функция отображения PopUp
 function PopUpShow(){
-    $("#popup1").show();   
-    $(".op").show();
-
+    $('#popup1').fadeTo(500, 1);   
+    $('.op').show();
 }
 //Функция скрытия PopUp
 function PopUpHide(){
-    $("#popup1").hide();
-    $(".op").hide();
+    $('#popup1').fadeOut();
+    $('.op').hide();
+    app.removeErrorAll();
+    $('input, textarea').val('');
+    $('#fileformlabel').empty();
+    document.getElementById('fileformlabel').innerHTML = "Загрузите изображение";
 }
+
 
 $(function() {
     var pull        = $('#pull');
@@ -34,7 +40,11 @@ $(function() {
             menu.removeAttr('style');
         }
     });
+
+
+
 });
+
 
 function getName (str){
     if (str.lastIndexOf('\\')){
@@ -44,6 +54,77 @@ function getName (str){
         var i = str.lastIndexOf('/')+1;
     }                       
     var filename = str.slice(i);            
-    var uploaded = document.getElementById("fileformlabel");
+    var uploaded = document.getElementById('fileformlabel');
     uploaded.innerHTML = filename;
 }
+
+     var app = {
+
+        init : function () {          
+            this.setUpListeners();
+        },
+
+        setUpListeners: function () {
+            $('form').on('submit', app.submitForm);
+            $('form').on('reset', app.removeErrorAll);
+            $('form').on('click', 'input, textarea, label', app.removeError);  
+        },
+
+        submitForm: function (e) {
+            e.preventDefault();
+
+            var form = $('form');
+            if (app.validateForm(form) === false) return false;
+            console.log('submitt!!')
+
+        },
+
+        validateForm: function(form) {
+            var inputs = form.find('input, textarea'),
+                valid =true;
+
+            app.removeErrorAll(); 
+
+            $.each(inputs, function(index, val) {
+                 var input = $(val),
+                     val = input.val(),
+                     child = input.parents('.input_group').children('div').children('.for_input');
+                     childImg = input.parents('.input_group').children('.label_img');
+
+                if(val.length === 0){
+                    child.addClass('has_error').removeClass('has_succes');
+                    childImg.addClass('has_error').removeClass('has_succes');                    
+                    child.parent('div').prepend('<div class="error">');
+                    childImg.prepend('<div class="error">');
+                    valid = false;
+                } else {
+                    child.addClass('has_succes').removeClass('has_error');
+                    childImg.addClass('has_succes').removeClass('has_error');
+                }
+            });
+            
+        },
+        removeError: function() {
+            $(this).removeClass('has_error');
+            $(this).siblings('.error').remove();
+        },
+
+        removeErrorAll: function() {
+            var form = $('form'),
+                inputs = form.find('input, textarea');
+
+             $.each(inputs, function() {
+                    $(this).removeClass('has_error').removeClass('has_succes');
+                    $(this).parent('label').removeClass('has_error').removeClass('has_succes');
+                    $('.error').remove();}
+                    )
+        },
+
+
+    }
+
+    app.init();
+
+
+
+
